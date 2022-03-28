adbf25f2c9893e5a843e8e168420dd7806a66233
Clinton Nolan <clinton.nolan@gmail.com>
2022-03-28 18:04:45 -0500

    Initial skeleton

---
+++ probe-droid-core/src/test/java/org/cnolan/RectangleMapTest.java
@@ -0,0 +1,15 @@
+package org.cnolan;
+
+import org.junit.jupiter.api.Test;
+
+public class RectangleMapTest {
+    @Test
+    void test(){
+        System.out.println("This test is running");
+    }
+
+    @Test
+    void test2(){
+        System.out.println("This 2nd test is running");
+    }
+}
