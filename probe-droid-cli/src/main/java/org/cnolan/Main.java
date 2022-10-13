9548e9bb9e2729afa8dd159f6f7d6c6fdfc65c38
Clinton Nolan <clinton.nolan@gmail.com>
2022-10-12 21:17:12 -0500

    Started work on using Spring Boot to implement the CLI

--- probe-droid-cli/src/main/java/org/cnolan/Main.java
+++ probe-droid-cli/src/main/java/org/cnolan/Main.java
@@ -2,6 +2,7 @@ package org.cnolan;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 import org.cnolan.output.DroidPositionWriter;
 import org.cnolan.output.OutputWriter;
@@ -12,20 +13,41 @@ import org.cnolan.parser.PlainDroidFormatStringParser;
 import org.cnolan.parser.RectangleMapParser;
 import org.cnolan.simulation.DroidSimulation;
 import org.cnolan.simulation.Simulation;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-public class Main{
+@SpringBootApplication
+public class Main {
 
-    public static void main(String[] args) throws IOException {
-        ParserReader parserReader = new ParserReader();
-        RectangleMapParser mapParser = new RectangleMapParser();
-        DroidParser droidParser = new DroidParser();
-        Parser<String> parser = new PlainDroidFormatStringParser(droidParser, mapParser, parserReader);
-        Simulation simulation = new DroidSimulation();
-        OutputWriter outputWriter = new DroidPositionWriter();
-        SimulationPipeline<String> sim = new SimulationPipeline<>(parser, simulation, outputWriter);
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 
-        String input = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            ParserReader parserReader = new ParserReader();
+            RectangleMapParser mapParser = new RectangleMapParser();
+            DroidParser droidParser = new DroidParser();
+            Parser<String> parser = new PlainDroidFormatStringParser(droidParser, mapParser, parserReader);
+            Simulation simulation = new DroidSimulation();
+            OutputWriter outputWriter = new DroidPositionWriter();
+            SimulationPipeline<String> sim = new SimulationPipeline<>(parser, simulation, outputWriter);
 
-        sim.run(input);
+            try (Scanner scanner = new Scanner(System.in)){
+                StringBuffer input = new StringBuffer();
+                
+                while(scanner.hasNextLine()){
+                    String line = scanner.nextLine();
+                    input.append(line);
+                    input.append("\n");
+                }
+
+                sim.run(input.toString());
+            }
+        };
     }
 }
